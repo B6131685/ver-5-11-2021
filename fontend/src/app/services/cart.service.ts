14f8893a8514236a4f63e5459cd19f1db72c0ed0
Issarapap7 <film4777@gmail.com>
2021-11-05 03:08:30 +0700

    ก่อนรุ่งสราง

---
+++ fontend/src/app/services/cart.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { cartsType } from '../cart.model';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+import { BookService } from 'src/app/services/book.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  carts : cartsType = [];
+  price: number = 0;
+
+  stop = false;
+  constructor(private http:HttpClient, private BookService:BookService) { }
+
+  getFromBuy(item:any){
+    this.carts.push(item) // เพิ่ม หนังสือเข้าไปเก็บ เป็น [{หนังสือ1},{หนังสือ2},{หนังสือ3}]
+    //console.log("From Cs ",this.carts) //
+    this.price += item.price //ร่วมราคา
+    //console.log("price fromBuy",this.price) //
+  }
+
+  deleteFormCart(item:any){
+    this.carts[item].quantity+=1; 
+    this.price -= this.carts[item].price;
+    console.log("price from delete"+this.price)
+    this.carts.splice(item,1);
+  }
+
+  getCart(){
+    return this.carts;
+  }
+
+  getTotal(){
+    return this.carts.length;
+  }
+
+  getTotalPrice(){
+    return this.price;
+  }
+
+  addOrder(product : any){
+    console.log('addOrder');
+    console.log(product);
+    return this.http.post<any>('http://localhost:3000/bookstore/addorder', product)
+    .pipe(map(data =>{
+      return data;
+    }))
+  }
+
+  deleteElementCart(item:any){
+    this.carts.forEach((element,index)=>{
+    if(this.stop == false){  
+      if(element==item){ 
+        this.price = this.price - item.price;
+        delete this.carts[index];
+        this.stop = true;   
+      };
+    }
+   }
+   );
+   this.stop = false;
+  }
+
+  resetCart(){
+    for (const element of this.carts) { // [{หนังสือ1},{หนังสือ2},{หนังสือ1}]
+      //console.log(element);
+      element.quantity = element.quantity -1;
+      this.updateMinusStock(element);
+    }
+    this.carts = [];
+    //console.log('after reset',this.carts);  // โหล่งจิง
+  }
+
+  updateMinusStock(item:any){  // item จะเป็นตัวที่ได้จาก for มาลบ
+    //console.log(item+"--> newQ = "+count);
+    try {
+      this.BookService.updateBook(item).subscribe(
+        data => {
+          //this.products = data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+}
---
+++ fontend/src/app/userMode/buy-products/buy-products.component.ts
@@ -0,0 +1,116 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, Input, OnInit,EventEmitter,Output } from '@angular/core';
+import { cartsType } from 'src/app/cart.model';
+import { BookService } from 'src/app/services/book.service';
+import { CartService } from 'src/app/services/cart.service';
+
+
+@Component({
+  selector: 'app-buy-products',
+  templateUrl: './buy-products.component.html',
+  styleUrls: ['./buy-products.component.css']
+})
+export class BuyProductsComponent implements OnInit {
+
+  @Input() item: any;
+  cart: cartsType = [];
+  show: boolean = true;
+  show1: boolean = true;
+  price: number =0;
+  count: number = 0;
+
+  constructor(private BookService:BookService,private CartService:CartService) {
+  }
+    
+  ngOnInit(): void {
+  }
+
+  addtoCart(){
+    console.log("add new product -->",this.item)
+    console.log(this.item); //รับมาจาก componet เลือกหนังสือ
+    if(this.item.quantity == 0){
+      this.show = false;
+     // this.count = 0; //น่าจะ นับจำนวนหนังสือของ component นี้
+    }else{
+      this.count +=1; 
+    //console.log(this.count)
+    this.CartService.getFromBuy(this.item);  // ส่งไปกดที่ตัวแปรร่วม
+    //this.updateMinusStock(this.item,1); //ลบไปแล้ว T T  ใจเย็นก่อนนนนน
+    }
+  }
+
+  checkaddTocart(){
+    if(this.item.quantity == this.count){
+      alert("ไม่สามารถเพิ่มได้มากกว่านี้แล้ว")
+    }else{
+      this.addtoCart();
+    }
+  }
+
+  deleteCart(){
+    //console.log("delete product",this.item)
+    if(this.count ==0){
+      //this.show1 = false;
+    }else{
+      this.count -=1;
+      this.CartService.deleteElementCart(this.item)
+      //this.updatePlusStock(this.item,1)    //อย่าพึ่งลบ ใจเยนน
+    }
+    
+  }
+
+  checkdeleteTocart(){
+    if(this.count == 0){
+      console.log('คุณไม่มีสินค้าในตะกร้า');
+      
+    }else{
+      this.deleteCart();
+    }
+  }
+
+  getTotal(){
+    return this.count;
+  }
+
+  updatePlusStock(item:any,count:any){ // count=1 เพราะ เซ็ทให้ส่ง 1 มาลบ
+    //console.log(item+"--> newQ = "+count);
+    if( count === undefined) count=0;
+    this.item.quantity = this.item.quantity + count;
+
+    try {
+      this.BookService.updateBook(this.item).subscribe(
+        data => {
+          //this.products = data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  updateMinusStock(item:any,count:any){
+    //console.log(item+"--> newQ = "+count);
+    if( count === undefined) count=0;
+    item.quantity = item.quantity - Number(count);
+    if(item.quantity == 0){
+      this.show = !this.show
+    }
+    try {
+      this.BookService.updateBook(this.item).subscribe(
+        data => {
+          //this.products = data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+}
